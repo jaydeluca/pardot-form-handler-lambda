b8061a9e81191a5ecf3e854cb4fd09a16b301580
jaydeluca <jaydeluca4@gmail.com>
2021-08-03 20:35:45 -0400

    consolidate example into a single file

--- example/main.tf
+++ example/main.tf
@@ -1,3 +1,29 @@
+variable "aws_account" {}
+variable "completion_redirect_url" {}
+variable "google_recaptcha_secret" {}
+variable "pardot_form_handler_url" {}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.52.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "pardot-form-handler-terraform-tfstate"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  allowed_account_ids = [var.aws_account]
+}
+
 // Handler
 module "pardot-form-handler" {
   source                  = "../"
--- example/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "aws_account" {}
-variable "completion_redirect_url" {}
-variable "google_recaptcha_secret" {}
-variable "pardot_form_handler_url" {}
